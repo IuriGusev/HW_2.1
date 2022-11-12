0b20639af607f97025998bc8cc47c26c1da4b927
IuriGusev <gusevyuv@gmail.com>
2022-11-12 12:23:25 +0300

    made hw_2.1_

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,50 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Task 1
+        // Присвоение значения переменной примитивного типа int 33. Измененяем значение переменной внутри метода ниже на 22.
+        // При выводе на "печать" изменения значения на 22 не происходит, так как изменение проиходится за пределами main.
+        // для присвоения нового значения следовало бы переменной присвоить значение метода changeValue (value);
         int value = 33;
         changeValue (value);
         System.out.println(value);
 
         //Task 2
+        // Присвоение значения переменной ссылочного типа Interger 33. Измененяем значение внутри метода ниже на 22.
+        // При выводе на "печать" изменения значения на 22 не происходит, так как изменение проиходится за пределами main.
+        // Для присвоения нового значения следовало бы также переменной присвоить значение метода changeValue (val);
         Integer val = 33;
         changeValue (val);
         System.out.println(val);
 
         //Task 3
+        // Присвоение значения массиву cсылочного типа Interger элементов 3 и 4. Измененяем значение массива внутри метода ниже на 1 и 2.
+        // При выводе на "печать" изменения значения на 1 и 2 не происходит, так как изменение проиходится за пределами main и
+        // новое значение из метода мы не используем.
         Integer [] integers = new Integer[] {3, 4};
         changeValue (integers);
         Arrays.toString(integers);
         System.out.println(Arrays.toString(integers));
 
         //Task 4
+        // Присвоение эелементам 0 и 1 массива cсылочного типа Interger значений 3 и 4. Измененяем значение ячейки 0 массива внутри метода ниже на 99.
+        // При выводе на "печать" происходит изменение значения 3 на 99, так как изменение проиходится внутри метода с реальным объектов и
+        // новое значение элемента метода мы как раз и используем.
         Integer [] numbers = new Integer[] {3, 4};
-        changeValue (numbers);
+        changeValue1 (numbers);
         Arrays.toString(numbers);
         System.out.println(Arrays.toString(numbers));
 
         //Task 5
+        // Присвоение значений нового объекта класса Person. Измененяем значение объектов внутри метода ниже.
+        // При выводе на "печать" изменения значения объектов не происходит, так как новое значение не используется.
         Person person = new Person ("Lyapis", "Trubetskoy");
         changePerson (person);
         System.out.println(person);
 
+        // Task 6
+        // Присвоение значения объектам класса Person. Измененяем значение объектов внутри метода ниже.
+        // При выводе на "печать" изменения значения объектов происходит, так как новое значение уже используется в результате
+        // изменения объекта по ссылке и использования сеттеров.
         person = new Person ("Lyapis", "Trubetskoy");
         changePerson1 (person);
         System.out.println(person);
@@ -51,6 +69,6 @@ public class Main {
         person = new Person ("Ilya", "Lagutenko"); }
 
     static void changePerson1 (Person person) {
-        person.getName ("Ilya");
-        person.getSurname ("Lagutenko"); }
+        person.setName("Ilya");
+        person.setSurname ("Lagutenko"); }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,15 +2,22 @@ public class Person {
     private String name;
     private String surname;
 
-    public Person (String name, String surname){
+    public Person(String name, String surname) {
         this.name = name;
-        this.surname = surname; }
+        this.surname = surname;
+    }
 
-    public String getName(String ilya) { return this.name; }
+       public void setName(String name) { this.name = name;    }
 
-    public String getSurname(String lagutenko) { return this.surname; }
+       public void setSurname (String surname) { this.surname = surname;    }
 
     @Override
-    public String toString() { return name + surname; }
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
+
 
